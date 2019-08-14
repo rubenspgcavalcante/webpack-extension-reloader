99470f2248c7e6475520c6b75722fe6485e1de56
Kadauchi <kadauchi@gmail.com>
2019-08-14 14:31:07 -0700

    Added popup as default extension page entry.

--- src/constants/options.constants.ts
+++ src/constants/options.constants.ts
@@ -3,3 +3,4 @@ export const DEFAULT_CONFIG = "webpack.config.js";
 export const DEFAULT_RELOAD_PAGE = true;
 export const DEFAULT_CONTENT_SCRIPT_ENTRY = "content-script";
 export const DEFAULT_BACKGROUND_ENTRY = "background";
+export const DEFAULT_EXTENSION_PAGE_ENTRY = "popup";
